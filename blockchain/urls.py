e91d08586da19d529fc68e76db238e5631e6cb27
akkupy <akkupy@proton.me>
2024-02-14 00:13:20 +0530

    Added Blockchain APIs

--- blockchain/urls.py
+++ blockchain/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-
+from .views import get_chain,mine_block,is_blockchain_valid
 
 urlpatterns = [
-    
+    path('get/', get_chain,name='get_chain'),
+    path('mine_block/', mine_block,name='mine_block'),
+    path('validate/', is_blockchain_valid,name='validate_chain'),
 ]
--- blockchain/views.py
+++ blockchain/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from blockchain.blockchain import Blockchain
+
+blockchain = Blockchain()
+
+
+@api_view(['GET'])
+def get_chain(request):
+    if not blockchain.is_chain_valid():
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    return Response(blockchain.chain)
+
+
+@api_view(['POST'])
+def mine_block(request):
+    data = request.data
+    if not blockchain.is_chain_valid():
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    return Response(blockchain.mine_block(data))
+
+@api_view(['GET'])
+def is_blockchain_valid(request):
+    if not blockchain.is_chain_valid():
+        return Response({"status": False})
+    return Response({"status": True})
