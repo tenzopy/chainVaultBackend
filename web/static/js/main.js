f44af1da8bf906c0b604b6ce1845bd4252b85779
akkupy <akkupy@proton.me>
2024-02-13 23:13:56 +0530

    Fixed bugs

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -2,57 +2,6 @@ $(document).ready(function() {
     "use strict";
     
     
-    /* ==========================
-	   JOIN-US FORM
-	=============================*/
-
-	$('#submit_btn').on('click', function() { 
-        var proceed = true;
-        //simple validation at client's end
-        //loop through each field and we simply change border color to red for invalid fields		
-		$("#join-us-form input[required=true]").each(function(){
-			$(this).css('border-color',''); 
-			if(!$.trim($(this).val())){ //if this field is empty 
-				$(this).css('border','solid 1px red'); //change border color to red   
-				proceed = false; //set do not proceed flag
-			}
-			//check invalid email
-			var email_reg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/; 
-			if($(this).attr("type")=="email" && !email_reg.test($.trim($(this).val()))){
-				$(this).css('border','solid 1px red'); //change border color to red   
-				proceed = false; //set do not proceed flag				
-			}	
-		});
-        if(proceed) //everything looks good! proceed...
-        {
-			//get input field values data to be sent to server
-            var post_data = {
-				'first_name'	: $('input[name=first_name]').val(),
-                'last_name'		: $('input[name=last_name]').val(),				
-				'user_email'	: $('input[name=email]').val(), 
-				'phone_number'	: $('input[name=phone]').val(), 
-				'website'		: $('input[name=website]').val()
-			};
-	        //Ajax post data to server
-            $.post('join_us.php', post_data, function(response){ 
-				if(response.type == 'error'){ //load json data from server and output message     
-					var output = '<div class="error">'+response.text+'</div>';
-				}else{
-				    output = '<div class="success">'+response.text+'</div>';
-					//reset values in all input fields
-					$("#join-us-form  input[required=true]").val(''); 
-				}
-				$("#join-us-results").hide().html(output).slideDown();
-            }, 'json');
-        }
-    });
-    
-    //reset previously set border colors and hide all message on .keyup()
-    $("#join-us-form  input[required=true]").keyup(function() { 
-        $(this).css('border-color',''); 
-        $("#join-us-results").slideUp();
-    });
-    
 
     // Anchor Smooth Scroll
     $('body').on('click', '.page-scroll', function(event) {
@@ -64,41 +13,6 @@ $(document).ready(function() {
     });
 
   
-
-    // Review
-    $('.review-text').slick({
-        dots: true,
-        infinite: true,
-        arrows: false,
-        speed: 300,
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        responsive: [{
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    dots: true
-                }
-            }, {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            }, {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
-        ]
-    });
 	
     // Video Lightbox
     $('.swipebox-video').swipebox();
@@ -148,46 +62,6 @@ $(document).ready(function() {
 
 });
 
-// Product Filter
-$(window).load(function() {
-    "use strict";
-    var $container = $('.portfolio-grid');
-    $container.isotope({
-        layoutMode: "masonry",
-        masonry: {
-            columnWidth: 5
-        },
-        itemSelector: '.portfolio-item',
-        transitionDuration: '0.8s'
-    });
-    var $optionSets = $('.portfolio-filter'),
-        $optionLinks = $optionSets.find('a');
-    $optionLinks.on(function() {
-        var $this = $(this);
-        // don't proceed if already selected
-        if ($this.hasClass('active')) {
-            return false;
-        }
-        var $optionSet = $this.parents('.portfolio-filter');
-        $optionSet.find('.active').removeClass('active');
-        $this.addClass('active');
-        // make option object dynamically, i.e. { filter: '.my-filter-class' }
-        var options = {},
-            key = $optionSet.attr('data-option-key'),
-            value = $this.attr('data-option-value');
-
-        // parse 'false' as false boolean
-        value = value === 'false' ? false : value;
-        options[key] = value;
-        if (key === 'layoutMode' && typeof changeLayoutMode === 'function') {
-            changeLayoutMode($this, options);
-        } else {
-            // otherwise, apply new options
-            $container.isotope(options);
-        }
-        return false;
-    });
-});
 
 
 $('.count').each(function() {
