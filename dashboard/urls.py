bceba4ecbe84b1ca911bd074acaf8e6010e48c98
akkupy <akkupy@proton.me>
2024-02-16 23:19:52 +0530

    Initilised dash

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .views import dash
+from .views import dash,upload,download,share
 
 urlpatterns = [
-    path('',dash,name='dashboard')
+    path('',dash,name='dashboard'),
+    path('upload/',upload),
+    path('download/',download),
+    path('share/',share),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -2,14 +2,20 @@ from django.shortcuts import render,HttpResponse
 
 from blockchain.views import blockchain
 
-# from hash_table.views import dhtnode
+from hash_table.views import DHT
 
 
 
 def dash(request):
 
     blockchain.replace_chain()
+    print(DHT.data)
 
-    print(blockchain.chain)
+def upload(request):
+    pass
 
-    return HttpResponse("hey")
+def download(request):
+    pass
+
+def share(request):
+    pass
