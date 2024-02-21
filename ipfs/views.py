70547ab7dd8aaad08f14e3fc7ef3a45b86eff99d
akkupy <akkupy@proton.me>
2024-02-21 22:18:59 +0530

    Switched IPFS ip to get from env

--- ipfs/views.py
+++ ipfs/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+import os
 from .ipfs import IPFS
 from django.conf import settings
+from dotenv import load_dotenv,find_dotenv
 
-ipfs = IPFS('ip4',settings.ALLOWED_HOSTS[0],5001)
+load_dotenv(find_dotenv())
+
+ipfs = IPFS('ip4',os.environ.get("IPFS_IP_ADDRESS"),5001)
 
