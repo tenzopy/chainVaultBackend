59e351fd3516be15bb114bf07de1db03d0b4a145
vismaya2002 <vismay2002@gmail.com>
2024-02-21 13:45:37 +0530

    addes file encryption using AES

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,30 +1,28 @@
 from django.shortcuts import render,HttpResponse
-
 from django.conf import settings
-
 from django.core.files.storage import FileSystemStorage
-
 from blockchain.views import blockchain
-
 from hash_table.views import DHT
-
 from ipfs.views import ipfs
-
+import pyAesCrypt
 import os
 
 fs = FileSystemStorage()
 
 def dash(request):
 
-    blockchain.replace_chain()
+    blockchain.replace_chain(),
     print(DHT.data)
 
 def upload(request):
     if request.method == 'POST' and request.FILES['myfile']:
         myfile = request.FILES['myfile']
+        password = request.POST.get('password')
         filename = fs.save(myfile.name, myfile)
-        ipfs_cid = ipfs.upload_to_ipfs(filename)
+        pyAesCrypt.encryptFile(os.path.join(settings.MEDIA_ROOT,filename), os.path.join(settings.MEDIA_ROOT,filename+ ".aes"), password)
+        ipfs_cid = ipfs.upload_to_ipfs(os.path.join(settings.MEDIA_ROOT,filename+ ".aes"))
         os.remove(os.path.join(settings.MEDIA_ROOT, filename))
+        os.remove(os.path.join(settings.MEDIA_ROOT, filename+ ".aes"))
         return render(request, 'uploadz.html', {
             'ipfs_cid' : ipfs_cid
         })
@@ -33,7 +31,10 @@ def upload(request):
 def download(request):
     if request.method == 'POST':
         cid = request.POST.get('CID')
-        ipfs.download_from_ipfs(cid,'bling.jpg')
+        password = request.POST.get('password')
+        ipfs.download_from_ipfs(cid,'bling.aes')
+        pyAesCrypt.decryptFile(os.path.join(settings.MEDIA_ROOT, "bling.aes"), os.path.join(settings.MEDIA_ROOT,"bling.jpg"), password)
+        os.remove(os.path.join(settings.MEDIA_ROOT, "bling.aes"))
         return render(request, 'downloadz.html', {
             'download_url': fs.url('bling.jpg')
         })
