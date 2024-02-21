46420a95ff906c2db11f1e14f846534d7c1512cc
akkupy <akkupy@proton.me>
2024-02-21 22:18:31 +0530

    Removed User Auth

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-from .views import signup,signin,signout,activate
+
 
 urlpatterns = [
-    path('signup/', signup,name='signup'),
-    path('login/', signin,name='login'),
-    path('logout/',signout,name='logout'),
-    path('activate/<uidb64>/<token>/',activate, name='activate'),  
+
 ]
