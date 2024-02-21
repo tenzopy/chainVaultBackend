7d1ed32279fe005872650ee07b5f7f0340b82255
vismaya2002 <vismay2002@gmail.com>
2024-02-21 12:39:52 +0530

    added code for file uploading and downloading

--- dashboard/views.py
+++ dashboard/views.py
@@ -10,6 +10,9 @@ from hash_table.views import DHT
 
 from ipfs.views import ipfs
 
+import os
+
+fs = FileSystemStorage()
 
 def dash(request):
 
@@ -19,16 +22,22 @@ def dash(request):
 def upload(request):
     if request.method == 'POST' and request.FILES['myfile']:
         myfile = request.FILES['myfile']
-        fs = FileSystemStorage()
         filename = fs.save(myfile.name, myfile)
-        uploaded_file_url = fs.url(filename)
+        ipfs_cid = ipfs.upload_to_ipfs(filename)
+        os.remove(os.path.join(settings.MEDIA_ROOT, filename))
         return render(request, 'uploadz.html', {
-            'uploaded_file_url': uploaded_file_url
+            'ipfs_cid' : ipfs_cid
         })
     return render(request, 'uploadz.html')
 
 def download(request):
-    pass
+    if request.method == 'POST':
+        cid = request.POST.get('CID')
+        ipfs.download_from_ipfs(cid,'bling.jpg')
+        return render(request, 'downloadz.html', {
+            'download_url': fs.url('bling.jpg')
+        })
+    return render(request, 'downloadz.html')
 
 def share(request):
     pass
