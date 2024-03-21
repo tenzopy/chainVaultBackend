5347322ba5688ebf118c2c002a1e711b780de792
akkupy <akkupy@proton.me>
2024-03-21 13:36:47 +0530

    Fixed User Duplication Error

--- chainvault/settings.py
+++ chainvault/settings.py
@@ -171,6 +171,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+AUTH_USER_MODEL = 'user.User'
+
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
